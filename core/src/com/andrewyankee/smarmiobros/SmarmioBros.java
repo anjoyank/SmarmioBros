45185aa6f73e2ce99a02a44d7b8a85b18f94606a
Andrew Yankee <anjoyank@gmail.com>
2021-04-26 10:28:23 -0400

    made screens game file next step HUD

--- core/src/com/andrewyankee/smarmiobros/SmarmioBros.java
+++ core/src/com/andrewyankee/smarmiobros/SmarmioBros.java
@@ -1,31 +1,29 @@
 package com.andrewyankee.smarmiobros;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.andrewyankee.smarmiobros.Screens.PlayScreen;
 
-public class SmarmioBros extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class SmarmioBros extends Game {
+	//sprite batch is container that holds imgs/textures pre render
+	//very memory intensive only have one spritebatch
+	public SpriteBatch batch;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		//delegates render to PlayScreen--whatever screen is active
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
